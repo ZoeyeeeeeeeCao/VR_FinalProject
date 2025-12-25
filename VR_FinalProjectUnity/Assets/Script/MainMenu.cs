e2638f106921511ae5f66f37f05e70ccf1d34182
toleubekakniet123-collab <toleubekakniet123@gmail.com>
2025-12-25 15:53:16 +0800

    audio

--- VR_FinalProjectUnity/Assets/Script/MainMenu.cs
+++ VR_FinalProjectUnity/Assets/Script/MainMenu.cs
@@ -3,8 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource bgm;
+
     public void Play()
     {
+        if (bgm != null) bgm.Stop();
         SceneManager.LoadScene("Level1");
     }
 }
