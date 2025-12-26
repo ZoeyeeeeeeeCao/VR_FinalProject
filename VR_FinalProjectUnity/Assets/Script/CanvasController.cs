58e9f75273729cfa6e622c517ac6c3d847ec30d4
reasonka <aydin.sartaeva@gmail.com>
2025-12-27 02:52:02 +0800

    try add canves

---
+++ VR_FinalProjectUnity/Assets/Script/CanvasController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public GameObject canvas; // 拖 Canvas 进来
+
+    // 显示 Canvas（如果你需要）
+    public void ShowCanvas()
+    {
+        canvas.SetActive(true);
+    }
+
+    // 关闭 Canvas（按钮点这个）
+    public void CloseCanvas()
+    {
+        canvas.SetActive(false);
+    }
+}
